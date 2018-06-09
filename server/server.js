ad863e44b20f51e950832e4630e57a09ca0c5457
Muyao Xu <muyaoxu@usc.edu>
2018-06-08 21:14:04 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,31 @@ app.get('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	//Validate id using isValid
+		// 404 - send back empty send
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	//findById
+		// success
+			// if todo - send it back
+			// if not todo - send back 404 with empty body
+		// error
+			// 400 - and send empty body back
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			res.status(404).send();
+		} else {
+			res.status(200).send({todo});
+		}
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
 
 app.listen(3000, () => {
 	console.log('Started on port 3000');
