aed237c5b65a0e51abf6b566fa3101f6bb9150e9
Muyao Xu <muyaoxu@usc.edu>
2018-06-24 16:34:21 -0700

    Add POST /user route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,75 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+// {
+// 	email: 'andrew@example.colm',
+// 	password: 'dfaskfl;nak;sfdnalnda',
+// 	tokens: [{
+// 		access: 'auth',
+// 		token: 'afsfdajgklsafdkasldfnaskld'
+// 	}]
+// }
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
 		minlength: 1,
-		trim: true
-	}
-});
+		trim: true,
+		unique: true,
+		// custom email validation
+		validate: {
+			// validator: (value) => {
+			// 	return validator.isEmail(value);
+			// },
+			validator: validator.isEmail,
+			message: `{VALUE} is not a valid email`
+		}
+	},
+	password: {
+		type: String,
+		require: true,
+		minlength: 6
+	},
+	// array, available in mongodb
+	tokens:[{
+		access: {
+			type: String,
+			require: true
+		},
+		token: {
+			type: String,
+			require: true
+		}
+	}]
+})
+
+// instance methods 
+// attached to a SCHEMA
+// arrow methods do not bind 'this' keyword
+UserSchema.methods.generateAuthToken = function () {
+	var user = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+	// console.log("token" + token);
+	// user.tokens = user.tokens.concat([{access, token}]);
+	user.tokens.push({access, token});
+	// the value will be passed as the next success case for the then call
+	return user.save().then(() => {
+		return token;
+	});
+}
+
+// override the method
+UserSchema.methods.toJSON = function () {
+	var user = this;
+	var userObject = user.toObject();
+
+	return _.pick(userObject, ['_id', 'email']);
+};
+
+var User = mongoose.model('User', UserSchema );
 
 module.exports = {User};
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 require('./config/config');
 
-
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -106,6 +105,21 @@ app.patch('/todos/:id', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send();
 	});
+});
+
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+		// res.send(user);
+	}).then((token) => {
+		// start with 'x': custom header
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
 });
 
 app.listen(port, () => {
