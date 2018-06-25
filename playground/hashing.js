f63ac00521c9b7fad0b18e2a0b4681101e2f7d17
Muyao Xu <muyaoxu@usc.edu>
2018-06-24 17:34:16 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '1234');			//2@ secret
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		// console.log(hash);
+	});
+});
 
-var decoded = jwt.verify(token, '1234');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$5pLlx85frepLmLUGSr8sFukOV0eToXD/ExMD9ql8SFKRGexZ1yzBO';
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+	console.log(result);
+});
+
+// ------------------------JWT
+// const {SHA256} = require('crypto-js');
+// const jwt = require('jsonwebtoken');
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '1234');			//2@ secret
+// console.log(token);
+
+// var decoded = jwt.verify(token, '1234');
+// console.log('decoded', decoded);
 
 
 // ------------------replaced by JWT
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
 // {
 // 	email: 'andrew@example.colm',
 // 	password: 'dfaskfl;nak;sfdnalnda',
@@ -90,6 +92,22 @@ UserSchema.methods.toJSON = function () {
 
 	return _.pick(userObject, ['_id', 'email']);
 };
+
+// mongoose middleware
+UserSchema.pre('save', function (next) {
+	var user = this;
+
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
 
 var User = mongoose.model('User', UserSchema );
 
