ebaea8c7faf012dd7a84c9c95c53ea2ea22e8566
Muyao Xu <muyaoxu@usc.edu>
2018-06-25 22:05:28 -0700

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -63,6 +63,19 @@ UserSchema.methods.generateAuthToken = function () {
 	});
 };
 
+// instance method
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+
+	return user.update({
+		$pull: {
+			tokens: {
+				token: token
+			}
+		}
+	});
+};
+
 // model method
 UserSchema.statics.findByToken = function (token) {
 	var User = this;
